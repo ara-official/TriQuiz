dd9e6bd2755288d5fc399095c2c0c70c67f09f8f
jaehunchoi <dydrms44@naver.com>
2021-02-23 00:03:22 +0900

    add controller (default)

--- BackEnd/triquiz-be/src/app.module.ts
+++ BackEnd/triquiz-be/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateQuizController } from './create-quiz/create-quiz.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CreateQuizController],
   providers: [AppService],
 })
 export class AppModule {}
