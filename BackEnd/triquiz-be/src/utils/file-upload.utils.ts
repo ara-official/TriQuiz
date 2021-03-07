1b45d2a7c4549d8d59785807a0f1e294a77f9527
jaehunchoi <dydrms44@naver.com>
2021-03-07 19:12:41 +0900

    add sha256 hash and check file exist before make file name

--- BackEnd/triquiz-be/src/utils/file-upload.utils.ts
+++ BackEnd/triquiz-be/src/utils/file-upload.utils.ts
@@ -2,58 +2,88 @@ import { extname } from 'path';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { createCipheriv, randomBytes, scrypt } from 'crypto';
 import { promisify } from 'util';
-
+import { generateKeyPair } from 'node:crypto';
 
 // Allow only images
 export const imageFileFilter = (req, file, callback) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return callback(
-            new HttpException(
-                'Only image files are allowed!',
-                HttpStatus.BAD_REQUEST,
-            ),
-            false,
-        );
-    }
-    callback(null, true);
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return callback(
+      new HttpException(
+        'Only image files are allowed!',
+        HttpStatus.BAD_REQUEST,
+      ),
+      false,
+    );
+  }
+  callback(null, true);
 };
 
 export const editFileName = async function (req, file, callback) {
-    const name = file.originalname.split('.')[0];
-    const fileExtName = extname(file.originalname);
-    var testFolder = '../image_upload/';
-    var fs = require('fs');
-    
-    var hash = 1;
-    var randomName;
-    for (let i = 0; i < name.length; i++) {
-        hash = hash * 27 + name[i];
-    }
-    randomName = Array(5)
-            .fill(null)
-            .map(() => Math.round(Math.random() * 10).toString(10))
-            .join('');
+  const name = file.originalname.split('.')[0];
+  const fileExtName = extname(file.originalname);
+  var testFolder = './image_upload/';
+  var fs = require('fs');
+  //C:\Users\jaehunchoi\TriQuiz\BackEnd\triquiz-be\src\utils\file-upload.utils.ts
+  //C:\Users\jaehunchoi\TriQuiz\BackEnd\triquiz-be\image_upload
+  var randomName;
+  const crypto = require('crypto');
 
-    callback(null, `${hash}${randomName}${fileExtName}`);
+  var hash = crypto.createHash('sha256').update(name).digest('base64');
+
+  hash = hash.replace(/\\/g, '');
+  hash = regExp(hash);
+  randomName = Array(5)
+    .fill(null)
+    .map(() => Math.round(Math.random() * 10).toString(10))
+    .join('');
+
+  while (true) {
+    var path = `${testFolder}${hash}${randomName}${fileExtName}`;
+    if (await fs.existsSync(path)) {
+      randomName = Array(5)
+        .fill(null)
+        .map(() => Math.round(Math.random() * 10).toString(10))
+        .join('');
+    } else {
+      break;
+    }
+  }
+  callback(null, `${hash}${randomName}${fileExtName}`);
 };
 export const storeDirectory = async function (req, file, callback) {
-    var testFolder = './image_upload/';
-    var fs = require('fs');
-    var folder_index = 10000;
-    var max_file_num = 10000;
-    fs.exists(testFolder + 'folderinfo.txt', function (exists) {
-        if (exists) {
-            fs.readFile(testFolder + 'folderinfo.txt', 'utf8', function (err, data) {
-                console.log(data);
-                folder_index = data;
-            });
-        }
-        else {
-            fs.writeFile(testFolder + 'folderinfo.txt', '0', 'utf8', function (error) { console.log('write end') });
-            folder_index = 10000;
-        }
-    });
-    folder_index = (folder_index - (folder_index % max_file_num)) / max_file_num;
-    callback(null, `${testFolder}${folder_index}`);
-
+  var testFolder = './image_upload/';
+  var fs = require('fs');
+  var folder_index = 10000;
+  var max_file_num = 10000;
+  fs.exists(testFolder + 'folderinfo.txt', function (exists) {
+    if (exists) {
+      fs.readFile(testFolder + 'folderinfo.txt', 'utf8', function (err, data) {
+        console.log(data);
+        folder_index = data;
+      });
+    } else {
+      fs.writeFile(
+        testFolder + 'folderinfo.txt',
+        '0',
+        'utf8',
+        function (error) {
+          console.log('write end');
+        },
+      );
+      folder_index = 10000;
+    }
+  });
+  folder_index = (folder_index - (folder_index % max_file_num)) / max_file_num;
+  callback(null, `${testFolder}${folder_index}`);
 };
+function regExp(str) {
+  var reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
+  //특수문자 검증
+  if (reg.test(str)) {
+    //특수문자 제거후 리턴
+    return str.replace(reg, '');
+  } else {
+    //특수문자가 없으므로 본래 문자 리턴
+    return str;
+  }
+}
